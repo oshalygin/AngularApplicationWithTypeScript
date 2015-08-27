289bae4c8c5b247a312533f82d77068c6f9a7ec9
Oleg Shalygin <oshalygin@gmail.com>
2015-08-26 22:08:20 -0700

    routing for receipts

---
+++ FT.Web/app_development/receipts/receipt.routes.js
@@ -0,0 +1,15 @@
+(function () {
+    angular.module("app.receipts")
+        .config(configuration);
+    function configuration($stateProvider, $urlProvider) {
+        //todo might not be necessary...research
+        $urlProvider.otherwise("/");
+        $stateProvider
+            .state("receipts", {
+            url: "/Receipts",
+            templateUrl: "app_deployment/receipts/receipts.html",
+            controller: "app.receipts.ReceiptsController",
+            controllerAs: "vm"
+        });
+    }
+})();
---
+++ FT.Web/app_development/receipts/receipt.routes.ts
@@ -0,0 +1,22 @@
+﻿((): void => {
+    angular.module("app.receipts")
+        .config(configuration);
+
+    function configuration(
+        $stateProvider: angular.ui.IStateProvider,
+        $urlProvider: angular.ui.IUrlRouterProvider) {
+        //todo might not be necessary...research
+        $urlProvider.otherwise("/");
+
+        $stateProvider
+            .state("receipts", {
+                url: "/Receipts",
+                templateUrl: "app_deployment/receipts/receipts.html",
+                controller: "app.receipts.ReceiptsController",
+                controllerAs: "vm"
+            });
+    }
+
+})();
+
+    
