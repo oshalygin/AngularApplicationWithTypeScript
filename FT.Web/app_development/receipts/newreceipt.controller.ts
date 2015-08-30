b2575e8a506f3c15a0e1117efc8aa21f6210ad83
Oleg Shalygin <oshalygin@gmail.com>
2015-08-29 22:31:55 -0700

    added toastr and started newReceipt page

---
+++ FT.Web/app_development/receipts/newreceipt.controller.ts
@@ -0,0 +1,49 @@
+﻿module app.receipts {
+
+
+
+    interface INewReceiptController {
+        title: string;
+        receipt: app.models.IFundTrackReceipt;
+    }
+
+    class NewReceiptController implements INewReceiptController {
+
+        title: string;
+        receipt: app.models.FundTrackReceipt;
+
+        static $inject = ["ReceiptResource"];
+        constructor(           
+            private receiptResource: app.services.IReceiptResource) {
+       
+            var vm = this;
+            vm.title = "New Receipt";
+
+            this.receipt = new app.models.FundTrackReceipt();
+
+
+        }
+
+
+        private saveNewReceipt(): void {
+            this.receiptResource.save(this.receipt,
+            () => {
+                //TODO: Toaster stuff
+            },
+            () => {
+                //TODO: Toaster stuff
+            });
+
+        }
+
+
+    }
+
+
+
+    angular.module("app.receipts")
+        .controller("app.receipts.NewReceiptController",
+        NewReceiptController);
+
+}
+
