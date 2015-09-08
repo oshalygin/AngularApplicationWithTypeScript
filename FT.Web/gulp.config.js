df8f45f7a4188bd14fec3ea47672cc475a0cfd18
Oleg Shalygin <oshalygin@gmail.com>
2015-09-07 21:06:35 -0700

    gulp changes and front page

--- FT.Web/gulp.config.js
+++ FT.Web/gulp.config.js
@@ -4,6 +4,8 @@
     var devPath = "./app_development/";
     var appJsDev = devPath + "**/*.js";
     var bowerJson = require("./bower.json");
+    var layout = "./Views/Shared/";
+    var layoutInjector = "./_Layout.cshtml";
 
     var config = {
         appJavaScriptFiles:
@@ -27,21 +29,39 @@
         appDeployFolder: deploymentPath,
         appDevFolder: devPath,
 
-        js: [
-            devPath + "**/*.module.js",
-            devPath + "**/*.js",
-            "!" + devPath + "**/*.spec.js"
-        ],
-
         bower: {
             json: bowerJson,
             directory: "./bower_components/",
             ignorePath: "../.."
-        }
+        },
+        
+
+        js: [
+            devPath + "**/*.model.js",
+            devPath + "**/*.module.js",
+            devPath + "**/*.core.js",
+            devPath + "**/app.js",
+            devPath + "**/app.run.js",
+            devPath + "**/*.config.js",
+            devPath + "**/*.services.js",
+            devPath + "**/*.routes.js",
+            devPath + "**/*.directive.js",
+            devPath + "**/*.widget.js",
+            devPath + "**/*.filter.js",
+            devPath + "**/*.controller.js",
+            devPath + "**/*.js",
+            "!" + devPath + "**/*.spec.js"
+        ],
+
+        layoutPage: layout,
+        layoutInjector: layoutInjector
+
+
+
 
     };
 
-    config.getWiredepOptions = function () {
+    config.getWiredepDefaultOptions = function () {
         var options = {
             bowerJson: config.bower.json,
             directory: config.bower.directory,
--- FT.Web/gulpfile.js
+++ FT.Web/gulpfile.js
@@ -1,102 +1,102 @@
 /// <binding />
-    var gulp = require("gulp");
-    var config = require("./gulp.config")();
-    var $ = require("gulp-load-plugins")({ lazy: true });
-    var del = require("del");
+var gulp = require("gulp");
+var config = require("./gulp.config")();
+var $ = require("gulp-load-plugins")({ lazy: true });
+var del = require("del");
+var wiredepModule = require("wiredep");
 
 
+gulp.task("help", $.taskListing);
+gulp.task("default", ["help"]);
 
-    gulp.task("help", $.taskListing);
-    gulp.task("default", ["help"]);
+gulp.task("jshint", function () {
+    log("** JSHint Check **");
+    return gulp
+        .src(config.appJavaScriptFiles)
+        .pipe($.jshint())
+        .pipe($.jshint.reporter("jshint-stylish", { verbose: true }))
+        //todo:  Step necessary to stop the build process in case of JSHint blows up
+        .pipe($.jshint.reporter("fail"));
 
-    gulp.task("jshint", function() {
-        log("** JSHint Check **");
-        return gulp
-            .src(config.appJavaScriptFiles)
-            .pipe($.jshint())
-            .pipe($.jshint.reporter("jshint-stylish", { verbose: true }))
-            //todo:  Step necessary to stop the build process in case of JSHint blows up
-            .pipe($.jshint.reporter("fail"));
+});
 
-    });
+gulp.task("tslint", function () {
+    log("** TSLint Check **");
 
-    gulp.task("tslint", function() {
-        log("** TSLint Check **");
+    return gulp
+        .src(config.appTsDev)
+        .pipe($.tslint())
+        .pipe($.tslint.report("verbose"));
 
-        return gulp
-            .src(config.appTsDev)
-            .pipe($.tslint())
-            .pipe($.tslint.report("verbose"));
+});
 
-    });
+gulp.task("clean-js", function (done) {
+    var delConfig = [].concat(config.appJsDepolyment);
+    clean(delConfig, done);
+});
 
-    gulp.task("clean-js", function(done) {
-        var delConfig = [].concat(config.appJsDepolyment);
-        clean(delConfig, done);
-    });
+gulp.task("transpile", ["clean-js"], function () {
+    log("** Transpiling TypeScript Files **");
 
-    gulp.task("transpile", ["clean-js"], function() {
-        log("** Transpiling TypeScript Files **");
+    var typescriptOptions = {
+        removeComments: true,
+        target: "ES5",
+        noImplicitAny: true
+    };
+    //todo: fix sourcemaps destination
+    return gulp
+        .src([config.appTsDev, config.tsTypingDefinitions])
+        //.pipe($.sourcemaps.init({loadMaps: true}))
+        .pipe($.typescript(typescriptOptions))
+        //.pipe($.sourcemaps.write({includeContent: false}))
+        .pipe(gulp.dest(config.appDeployFolder));
+});
 
-        var typescriptOptions = {
-            removeComments: true,
-            target: "ES5",
-            noImplicitAny: true
-        };
-        //todo: fix sourcemaps destination
-        return gulp
-            .src([config.appTsDev, config.tsTypingDefinitions])
-            //.pipe($.sourcemaps.init({loadMaps: true}))
-            .pipe($.typescript(typescriptOptions))
-            //.pipe($.sourcemaps.write({includeContent: false}))
-            .pipe(gulp.dest(config.appDeployFolder));
-    });
+gulp.task("typescript-watcher", function () {
+    log("*** Watching TypeScript files for changes **");
 
-    gulp.task("typescript-watcher", function() {
-        log("*** Watching TypeScript files for changes **");
-
-        gulp.watch([config.appTsDev], ["transpile"])
-                .on("change", function (event) {
+    gulp.watch([config.appTsDev], ["transpile"])
+            .on("change", function (event) {
                 changedEvent(event);
             });
 
-    });
+});
 
-    gulp.task("wiredep", function() {
-        log("*** Bower CSS/JavaScript and Application JavasScript to _Layout.cshtml ***");
-        var options = config.getWiredepDefaultOptions();
-        var wiredep = require("wiredep").stream;
+gulp.task("wiredep", function () {
+    log("*** Bower CSS/JavaScript and Application JavasScript to _Layout.cshtml ***");
+    var options = config.getWiredepDefaultOptions();
+    var wiredep = wiredepModule.stream;
 
-        return gulp
-            .src(config.index)
-            .pipe(wiredep(options))
-            .pipe($.inject(gulp.src(config.js)))
-            .pipe(gulp.dest(config.devPath));
-    });
+    return gulp
+        .src(config.layoutInjector)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.js)))
+        .pipe(gulp.dest(config.layoutPage));
+});
 
-//todo add gulp-inject to populate the cshtml file...
+
 //todo add uglify/minification
-    
-    function clean(path, done) {
-        log("Cleaning " + $.util.colors.red(path));
-        del(path, done);
 
-    }
+function clean(path, done) {
+    log("Cleaning " + $.util.colors.red(path));
+    del(path, done);
 
-    function changedEvent(event) {
-        var srcPattern = new RegExp("/.*(?=/" + config.source + ")/");
-        var message = "File: " + event.path.replace(srcPattern, "") + " was " + event.type;
-        $.util.log($.util.colors.green(message));
-    }
+}
 
-    function log(msg) {
-        if (typeof (msg) === "object") {
-            for (var item in msg) {
-                if (msg.hasOwnProperty(item)) {
-                    $.util.log($.util.colors.blue(msg[item]));
-                }
+function changedEvent(event) {
+    var srcPattern = new RegExp("/.*(?=/" + config.source + ")/");
+    var message = "File: " + event.path.replace(srcPattern, "") + " was " + event.type;
+    $.util.log($.util.colors.green(message));
+}
+
+function log(msg) {
+    if (typeof (msg) === "object") {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
             }
-        } else {
-            $.util.log($.util.colors.blue(msg));
         }
+    } else {
+        $.util.log($.util.colors.blue(msg));
     }
+}
